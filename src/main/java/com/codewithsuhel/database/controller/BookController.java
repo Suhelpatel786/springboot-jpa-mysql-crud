57f9908e5c3ecc3a308806d6b12d2371fc4b771e
Suhel Patel <suhel.softcolon@gmail.com>
2025-05-15 11:50:44 +0530

    feat: implement update and delete APIs for Book, remove unnecessary cascade from Book.author

--- src/main/java/com/codewithsuhel/database/controller/BookController.java
+++ src/main/java/com/codewithsuhel/database/controller/BookController.java
@@ -2,10 +2,8 @@ package com.codewithsuhel.database.controller;
 
 import com.codewithsuhel.database.domain.Book;
 import com.codewithsuhel.database.services.BookServices;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -13,17 +11,27 @@ public class BookController {
 
     BookServices bookServices;
 
-    public BookController(BookServices bookServices){
+    public BookController(BookServices bookServices) {
         this.bookServices = bookServices;
     }
 
     @GetMapping("/books")
-    public Iterable<Book> getAllBooks(){
-         return bookServices.getAllBooks();
+    public Iterable<Book> getAllBooks() {
+        return bookServices.getAllBooks();
     }
 
     @PostMapping("/books")
-    public Book addBook(@RequestBody Book book){
+    public Book addBook(@RequestBody Book book) {
         return bookServices.addBook(book);
+    }
+
+    @PutMapping("/books/{isbn}")
+    public ResponseEntity<Book> updateBook(@PathVariable String isbn, @RequestBody Book book) {
+        return ResponseEntity.ok(bookServices.updateBook(isbn, book));
+    }
+
+    @DeleteMapping("/books/{isbn}")
+    public ResponseEntity<Book> deleteBook(@PathVariable String isbn) {
+        return ResponseEntity.ok(bookServices.deleteBook(isbn));
     }
 }
--- src/main/java/com/codewithsuhel/database/services/BookServices.java
+++ src/main/java/com/codewithsuhel/database/services/BookServices.java
@@ -4,8 +4,6 @@ import com.codewithsuhel.database.domain.Book;
 import com.codewithsuhel.database.repositories.BookRepositories;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
 public class BookServices {
     BookRepositories repositories;
@@ -21,4 +19,20 @@ public class BookServices {
     public Book addBook(Book book) {
         return repositories.save(book);
     }
+
+    public Book updateBook(String id, Book book) {
+        return repositories.findById(id).map(bookDetails -> {
+            bookDetails.setAuthor(book.getAuthor());
+            bookDetails.setTitle(book.getTitle());
+            return repositories.save(bookDetails);
+        }).orElseThrow(() -> new RuntimeException("Book not found with isbn: " + book.getIsbn()));
+    }
+
+    public Book deleteBook(String id) {
+        Book book = repositories.findById(id).orElseThrow(() -> new RuntimeException("Book not found with isbn: " + id
+        ));
+        repositories.deleteById(id);
+        return book;
+    }
+
 }
