320a85b00789d19ae2095a323f4c1e4c9a6e7d77
Suhel Patel <suhel.softcolon@gmail.com>
2025-05-15 12:09:31 +0530

    feat: implemented add and get authors

--- src/main/java/com/codewithsuhel/database/controller/AuthorController.java
+++ src/main/java/com/codewithsuhel/database/controller/AuthorController.java
@@ -1,6 +1,32 @@
 package com.codewithsuhel.database.controller;
 
 
+import com.codewithsuhel.database.domain.Author;
+import com.codewithsuhel.database.services.AuthorServices;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Iterator;
+
+@RestController
 public class AuthorController {
 
+    private final AuthorServices authorServices;
+
+    public AuthorController(AuthorServices authorServices) {
+        this.authorServices = authorServices;
+    }
+
+    @GetMapping("authors")
+    public ResponseEntity<Iterable<Author>> getAllAuthors() {
+        return ResponseEntity.ok(authorServices.getAllAuthors());
+    }
+
+    @PostMapping("authors")
+    public ResponseEntity<Author> addAuthor(@RequestBody Author author) {
+        return ResponseEntity.ok(authorServices.addAuthor(author));
+    }
 }
--- src/main/java/com/codewithsuhel/database/services/AuthorServices.java
+++ src/main/java/com/codewithsuhel/database/services/AuthorServices.java
@@ -4,6 +4,8 @@ import com.codewithsuhel.database.domain.Author;
 import com.codewithsuhel.database.repositories.AuthorRepositories;
 import org.springframework.stereotype.Service;
 
+import java.util.Iterator;
+
 @Service
 public class AuthorServices {
 
@@ -13,6 +15,12 @@ public class AuthorServices {
         this.repositories = authorRepositories;
     }
 
+    public Iterable<Author> getAllAuthors(){
+        return repositories.findAll();
+    }
 
+    public Author addAuthor(Author author){
+        return repositories.save(author);
+    }
 
 }
